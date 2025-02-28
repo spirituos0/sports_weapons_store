ccebd5083ab99e575a7d6c6c9a7d2eca93745d50
ivan.puhachov <puhachov@ut.ee>
2025-02-28 19:30:19 +0200

    Добавлен фронтенд для авторизации

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:goblin0001@localhost/shop_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "mysecretkey")  # Секретный ключ для JWT
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
